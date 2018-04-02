ba8993ec526f461584258d347171bfbaca9affab
stooggle <stuartodonnell@gmail.com>
2018-04-02 17:49:01 +0100

    First Assertion Passing

---
+++ Part_A/codeclan_student.rb
@@ -0,0 +1,14 @@
+
+
+class CodeclanStudent
+
+  def initialize(student_name, student_cohort)
+    @student_name = student_name
+    @student_cohort = student_cohort
+  end
+
+  def student_name
+    return @student_name
+  end
+
+end
